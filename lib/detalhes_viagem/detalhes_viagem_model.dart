5a8fa396cbc4365a110fc1992600fae3d9129181
Léo Moraes <leomoraesitu@hotmail.com>
2025-08-25 16:05:05 -0300

    feat(scr): Adiciona a tela DetalhesViagem (#6)
    Co-authored-by: FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>

---
+++ lib/detalhes_viagem/detalhes_viagem_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'detalhes_viagem_widget.dart' show DetalhesViagemWidget;
+import 'package:flutter/material.dart';
+
+class DetalhesViagemModel extends FlutterFlowModel<DetalhesViagemWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
--- lib/index.dart
+++ lib/index.dart
@@ -1,2 +1,3 @@
 // Export pages
 export '/lista_viagens/lista_viagens_widget.dart' show ListaViagensWidget;
+export '/detalhes_viagem/detalhes_viagem_widget.dart' show DetalhesViagemWidget;
