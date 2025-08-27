e1e35f9eab8d2705ef139517d4d159285fd5a299
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-08-27 22:49:11 +0000

    feat(scr): Adiciona a tela EditarViagem

---
+++ lib/shared_u_i/primary_image/primary_image_model.dart
@@ -0,0 +1,11 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'primary_image_widget.dart' show PrimaryImageWidget;
+import 'package:flutter/material.dart';
+
+class PrimaryImageModel extends FlutterFlowModel<PrimaryImageWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
