bad6e8366e322e5adf85624e5b91a4a1e67f5c14
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-08-28 00:18:38 +0000

    feat: Conecta a Tela "ListaViagens" ao BD

--- lib/pages/lista_viagens_page/lista_viagens_page_model.dart
+++ lib/pages/lista_viagens_page/lista_viagens_page_model.dart
@@ -1,27 +1,11 @@
 import '/flutter_flow/flutter_flow_util.dart';
-import '/shared_u_i/primary_rating_bar/primary_rating_bar_widget.dart';
 import 'lista_viagens_page_widget.dart' show ListaViagensPageWidget;
 import 'package:flutter/material.dart';
 
 class ListaViagensPageModel extends FlutterFlowModel<ListaViagensPageWidget> {
-  ///  State fields for stateful widgets in this page.
-
-  // Model for PrimaryRatingBar component.
-  late PrimaryRatingBarModel primaryRatingBarModel1;
-  // Model for PrimaryRatingBar component.
-  late PrimaryRatingBarModel primaryRatingBarModel2;
+  @override
+  void initState(BuildContext context) {}
 
   @override
-  void initState(BuildContext context) {
-    primaryRatingBarModel1 =
-        createModel(context, () => PrimaryRatingBarModel());
-    primaryRatingBarModel2 =
-        createModel(context, () => PrimaryRatingBarModel());
-  }
-
-  @override
-  void dispose() {
-    primaryRatingBarModel1.dispose();
-    primaryRatingBarModel2.dispose();
-  }
+  void dispose() {}
 }
