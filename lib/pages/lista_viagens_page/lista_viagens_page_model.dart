a93514e30a45818243a95fd1e1e2e40d9deb7fe6
Léo Moraes <leomoraesitu@hotmail.com>
2025-08-28 00:32:33 -0300

    feat: Adiciona navigation e conecta a tela "DetalhesViagem" ao BD (#15)
    * feat(scr): Adiciona a tela EditarViagem
    
    * feat: Conecta a Tela "ListaViagens" ao BD
    
    * feat: Adiciona navigation e conecta tela "DetalhesViagem" ao BD
    
    ---------
    
    Co-authored-by: FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>

--- lib/pages/lista_viagens_page/lista_viagens_page_model.dart
+++ lib/pages/lista_viagens_page/lista_viagens_page_model.dart
@@ -1,11 +1,28 @@
 import '/flutter_flow/flutter_flow_util.dart';
+import '/shared_u_i/primary_rating_bar/primary_rating_bar_widget.dart';
+import '/index.dart';
 import 'lista_viagens_page_widget.dart' show ListaViagensPageWidget;
 import 'package:flutter/material.dart';
 
 class ListaViagensPageModel extends FlutterFlowModel<ListaViagensPageWidget> {
-  @override
-  void initState(BuildContext context) {}
+  ///  State fields for stateful widgets in this page.
+
+  // Models for PrimaryRatingBar dynamic component.
+  late FlutterFlowDynamicModels<PrimaryRatingBarModel> primaryRatingBarModels1;
+  // Models for PrimaryRatingBar dynamic component.
+  late FlutterFlowDynamicModels<PrimaryRatingBarModel> primaryRatingBarModels2;
 
   @override
-  void dispose() {}
+  void initState(BuildContext context) {
+    primaryRatingBarModels1 =
+        FlutterFlowDynamicModels(() => PrimaryRatingBarModel());
+    primaryRatingBarModels2 =
+        FlutterFlowDynamicModels(() => PrimaryRatingBarModel());
+  }
+
+  @override
+  void dispose() {
+    primaryRatingBarModels1.dispose();
+    primaryRatingBarModels2.dispose();
+  }
 }
